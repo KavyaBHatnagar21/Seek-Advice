30492142640cc6b105e5eb9b00fdfdeadf162478
Kavya Bhatnagar <68288233+KavyaBHatnagar21@users.noreply.github.com>
2021-05-31 21:51:11 +0530

    final Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [advice, setAdvice] = useState("");
+  const [randomNumber, setRandomNumber] = useState(0);
+  const generalAdvice = [
+    "You should meditate the answer lies within you.",
+    "Ask your mom, she knows better.",
+    "Talk to your friend about it.",
+  ];
+
+  const fireQuery = (e) => {
+    e.preventDefault();
+    setSearch(search);
+    fetch(`https://api.adviceslip.com/advice/search/${search}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log("result: ", result);
+          if (!result.hasOwnProperty("slips")) {
+            setRandomNumber(Math.floor(Math.random() * 3));
+            console.log(
+              "Current random advice : ",
+              generalAdvice[randomNumber]
+            );
+            setAdvice("null");
+          } else {
+            setAdvice(result.slips[0].advice);
+          }
+        },
+        (error) => {}
+      );
+    setSearch("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Seek Advice and Keep Calm</h1>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLB3ZFUBeO9EEy0kLrwta-g4XXyjqa-Q177g&usqp=CAU"
+        alt=""
+      />
+      <form>
+        <input
+          className="search__box"
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button className="search__button" type="submit" onClick={fireQuery}>
+          Search
+        </button>
+      </form>
+      <div className="advice">
+        <h3>{advice === "null" ? generalAdvice[randomNumber] : advice}</h3>
+      </div>
     </div>
   );
 }
